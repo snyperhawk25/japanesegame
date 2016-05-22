5d056dd57fcd6a1baddfbde7fb04476fe655a8d2
snyperhawk25 <barrettsharpe25@gmail.com>
2016-05-22 13:12:34 -0700

    Added the question index to the japanese text (.qj element), and commented our the original question element, .q , in order to make sure it is not being used anywhere else.

--- questions.lua
+++ questions.lua
@@ -1,6 +1,7 @@
 
 
 --------------------------------------------------------------------------------
+--LinguisticVerification Branch
 -- GAME 2 QUESTIONS (questions[] 0 to 9)
 --------------------------------------------------------------------------------
 
@@ -27,8 +28,8 @@ question = {}
  -- bag
 question[0] = {}
 
-question[0].qj = "かばん　は　どれですか ?"
-question[0].q = syl[11][2] .. hiragana["ba"] .. hiragana["n"] .. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[0].qj = "Q0) かばん　は　どれですか ?"
+--question[0].q = syl[11][2] .. hiragana["ba"] .. hiragana["n"] .. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[0].a1 = "art/Game2/bag.png"
 question[0].a2 = "art/Game2/book.png"
 question[0].a3 = "art/Game2/door.png"
@@ -41,8 +42,8 @@ question[0].audio = "audio/kaban.wav"
  -- book
 question[1] = {}
 
-question[1].qj = "ほん　は　どれですか？"
-question[1].q = hiragana["ho"] .. hiragana["n"] .. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[1].qj = "Q1) ほん　は　どれですか？"
+--question[1].q = hiragana["ho"] .. hiragana["n"] .. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[1].a1 = "art/Game2/door.png"
 question[1].a2 = "art/Game2/book.png"
 question[1].a3 = "art/Game2/bag.png"
@@ -55,8 +56,8 @@ question[1].audio = "audio/hon.ogg"
  -- clock
 question[2] = {}
 
-question[2].qj = "とけい　は　どれですか？"
-question[2].q = hiragana["to"] .. hiragana["ke"] .. hiragana["i"].. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " ..  hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[2].qj = "Q2) とけい　は　どれですか？"
+--question[2].q = hiragana["to"] .. hiragana["ke"] .. hiragana["i"].. " " .. hiragana["wa"] .. " " .. hiragana["ha"] .. " " ..  hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[2].a1 = "art/Game2/window.png"
 question[2].a2 = "art/Game2/clock.png"
 question[2].a3 = "art/Game2/book.png"
@@ -69,8 +70,8 @@ question[2].audio = "audio/tokei.ogg"
  -- door
 question[3] = {}
 
-question[3].qj = "ドア　は　どれですか？"
-question[3].q = hiragana["do"] .. hiragana["a"] .. " " .. hiragana["ha"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[3].qj = "Q3) ドア　は　どれですか？"
+--question[3].q = hiragana["do"] .. hiragana["a"] .. " " .. hiragana["ha"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[3].a1 = "art/Game2/bag.png"
 question[3].a2 = "art/Game2/book.png"
 question[3].a3 = "art/Game2/door.png"
@@ -83,9 +84,9 @@ question[3].audio = "audio/doa.wav"
  -- hat
 question[4] = {}
 
-question[4].qj = "ぼうし　は　どれですか？"
+question[4].qj = "Q4) ぼうし　は　どれですか？"
 -- question4.q = hiragana["bo"] .. hiragana["us"] .. hiragana["hi"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
-question[4].q = "door" .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" -- Incorrect Japanese!
+--question[4].q = "door" .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" -- Incorrect Japanese!
 question[4].a1 = "art/Game2/umbrella.png"
 question[4].a2 = "art/Game2/clock.png"
 question[4].a3 = "art/Game2/door.png"
@@ -98,8 +99,8 @@ question[4].audio = "audio/boushi.wav"
  -- letter
 question[5] = {}
 
-question[5].qj = "てがみ　は　どれですか ?"
-question[5].q = hiragana["te"] .. hiragana["ga"] .. hiragana["mi"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[5].qj = "Q5) てがみ　は　どれですか ?"
+--question[5].q = hiragana["te"] .. hiragana["ga"] .. hiragana["mi"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[5].a1 = "art/Game2/letter.png"
 question[5].a2 = "art/Game2/wallet.png"
 question[5].a3 = "art/Game2/umbrella.png"
@@ -112,8 +113,8 @@ question[5].audio = "audio/tegami.wav"
  -- telephone
 question[6] = {}
 
-question[6].qj = "でんわ　は　どれですか？"
-question[6].q = hiragana["de"] .. hiragana["n"] .. hiragana["wa"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[6].qj = "Q6) でんわ　は　どれですか？"
+--question[6].q = hiragana["de"] .. hiragana["n"] .. hiragana["wa"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[6].a1 = "art/Game2/bag.png"
 question[6].a2 = "art/Game2/telephone.png"
 question[6].a3 = "art/Game2/window.png"
@@ -126,8 +127,8 @@ question[6].audio = "audio/denwa.wav"
  -- umbrella
 question[7] = {}
 
-question[7].qj = "かさ　は　どれですか？"
-question[7].q = hiragana["ka"] .. hiragana["sa"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[7].qj = "Q7) かさ　は　どれですか？"
+--question[7].q = hiragana["ka"] .. hiragana["sa"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[7].a1 = "art/Game2/clock.png"
 question[7].a2 = "art/Game2/book.png"
 question[7].a3 = "art/Game2/umbrella.png"
@@ -140,8 +141,8 @@ question[7].audio = "audio/kasa.wav"
  -- wallet
 question[8] = {}
 
-question[8].qj = "さいふ　は　どれですか？"
-question[8].q = hiragana["sa"] .. hiragana["i"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[8].qj = "Q8) さいふ　は　どれですか？"
+--question[8].q = hiragana["sa"] .. hiragana["i"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[8].a1 = "art/Game2/bag.png"
 question[8].a2 = "art/Game2/door.png"
 question[8].a3 = "art/Game2/wallet.png"
@@ -154,8 +155,8 @@ question[8].audio = "audio/saifu.wav"
  -- window
 question[9] = {}
 
-question[9].qj = "まど　は　どれですか？"
-question[9].q = hiragana["ma"] .. hiragana["do"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
+question[9].qj = "Q9) まど　は　どれですか？"
+--question[9].q = hiragana["ma"] .. hiragana["do"] .. " " .. hiragana["wa"] .. " " .. hiragana["do"] .. hiragana["re"] .. " " .. hiragana["de"] .. hiragana["su"] .. " " .. hiragana["ka"] .. "?" 
 question[9].a1 = "art/Game2/window.png"
 question[9].a2 = "art/Game2/umbrella.png"
 question[9].a3 = "art/Game2/bag.png"
@@ -172,8 +173,8 @@ question[9].audio = "audio/mado.wav"
  -- bread
 question[10] = {}
 
-question[10].qj = "ぱん　を　ください！"
-question[10].q = hiragana["pa"] .. hiragana["n"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[10].qj = "Q10) ぱん　を　ください！"
+--question[10].q = hiragana["pa"] .. hiragana["n"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[10].a1 = "art/Game3/bread.png"
 question[10].a2 = "art/Game3/coffee.png"
 question[10].a3 = "art/Game3/rice.png"
@@ -186,8 +187,8 @@ question[10].audio = "audio/pan.ogg"
  -- coffee
 question[11] = {}
 
-question[11].qj = "コーヒー　を　ください！"
-question[11].q = hiragana["ko"] .. hiragana["hi"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[11].qj = "Q11) コーヒー　を　ください！"
+--question[11].q = hiragana["ko"] .. hiragana["hi"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[11].a1 = "art/Game3/fish.png"
 question[11].a2 = "art/Game3/coffee.png"
 question[11].a3 = "art/Game3/hamburger.png"
@@ -200,8 +201,8 @@ question[11].audio = "audio/kohi.ogg"
  -- fish
 question[12] = {}
 
-question[12].qj = "さかな　を　ください！"
-question[12].q = hiragana["sa"] .. hiragana["ka"] .. hiragana["na"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[12].qj = "Q12) さかな　を　ください！"
+--question[12].q = hiragana["sa"] .. hiragana["ka"] .. hiragana["na"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[12].a1 = "art/Game3/water.png"
 question[12].a2 = "art/Game3/coffee.png"
 question[12].a3 = "art/Game3/fish.png"
@@ -214,8 +215,8 @@ question[12].audio = "audio/fish.ogg"
  -- hamburger
 question[13] = {}
 
-question[13].qj = "ハンバーガー　を　ください！"
-question[13].q = hiragana["ha"] .. hiragana["n"] .. hiragana["ba"] .. hiragana["ga"] .." " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[13].qj = "Q13) ハンバーガー　を　ください！"
+--question[13].q = hiragana["ha"] .. hiragana["n"] .. hiragana["ba"] .. hiragana["ga"] .." " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[13].a1 = "art/Game3/hamburger.png"
 question[13].a2 = "art/Game3/coffee.png"
 question[13].a3 = "art/Game3/meat.png"
@@ -228,8 +229,8 @@ question[13].audio = "audio/hanbaga.ogg"
  -- meat
 question[14] = {}
 
-question[14].qj = "にく　を　ください！"
-question[14].q = hiragana["ni"] .. hiragana["ku"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[14].qj = "Q14) にく　を　ください！"
+--question[14].q = hiragana["ni"] .. hiragana["ku"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[14].a1 = "art/Game3/coffee.png"
 question[14].a2 = "art/Game3/meat.png"
 question[14].a3 = "art/Game3/rice.png"
@@ -242,8 +243,8 @@ question[14].audio = "audio/meat.ogg"
  -- noodles
 question[15] = {}
 
-question[15].qj = "ラーメン　を　ください！"
-question[15].q = hiragana["ra"] .. hiragana["me"] .. " " .. hiragana["n"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[15].qj = "Q15) ラーメン　を　ください！"
+--question[15].q = hiragana["ra"] .. hiragana["me"] .. " " .. hiragana["n"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[15].a1 = "art/Game3/noodles.png"
 question[15].a2 = "art/Game3/meat.png"
 question[15].a3 = "art/Game3/bread.png"
@@ -256,8 +257,8 @@ question[15].audio = "audio/ramen.ogg"
  -- rice
 question[16] = {}
 
-question[16].qj = "ごはん　を　ください！"
-question[16].q = hiragana["go"] .. hiragana["ha"] .. hiragana["n"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[16].qj = "Q16) ごはん　を　ください！"
+--question[16].q = hiragana["go"] .. hiragana["ha"] .. hiragana["n"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[16].a1 = "art/Game3/water.png"
 question[16].a2 = "art/Game3/tea.png"
 question[16].a3 = "art/Game3/rice.png"
@@ -270,8 +271,8 @@ question[16].audio = "audio/rice.ogg"
  -- salad
 question[17] = {}
 
-question[17].qj = "サラダ　を　ください！"
-question[17].q = hiragana["sa"] .. hiragana["ra"] .. hiragana["da"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[17].qj = "Q17) サラダ　を　ください！"
+--question[17].q = hiragana["sa"] .. hiragana["ra"] .. hiragana["da"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[17].a1 = "art/Game3/noodles.png"
 question[17].a2 = "art/Game3/salad.png"
 question[17].a3 = "art/Game3/coffee.png"
@@ -284,8 +285,8 @@ question[17].audio = "audio/ramen.ogg" -- No audio for Sarada was given, current
  -- tea
 question[18] = {}
 
-question[18].qj = "おちゃ　を　ください！"
-question[18].q = "" .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" -- Needs to be changed to Japanese!
+question[18].qj = "Q18) おちゃ　を　ください！"
+--question[18].q = "" .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" -- Needs to be changed to Japanese!
 question[18].a1 = "art/Game3/rice.png"
 question[18].a2 = "art/Game3/coffee.png"
 question[18].a3 = "art/Game3/tea.png"
@@ -298,8 +299,8 @@ question[18].audio = "audio/ocha.ogg"
  -- vegetables
 question[19] = {}
 
-question[19].qj = "やさい　を　ください！"
-question[19].q = hiragana["ya"] .. hiragana["sa"] .. hiragana["i"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[19].qj = "Q19) やさい　を　ください！"
+--question[19].q = hiragana["ya"] .. hiragana["sa"] .. hiragana["i"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[19].a1 = "art/Game3/vegetables.png"
 question[19].a2 = "art/Game3/water.png"
 question[19].a3 = "art/Game3/tea.png"
@@ -312,8 +313,8 @@ question[19].audio = "audio/yasai.ogg"
  -- water
 question[20] = {}
 
-question[20].qj = "みず　を　ください！"
-question[20].q = hiragana["mi"] .. hiragana["zu"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
+question[20].qj = "Q20) みず　を　ください！"
+--question[20].q = hiragana["mi"] .. hiragana["zu"] .. " " .. hiragana["o"] .. " " .. hiragana["ku"] .. hiragana["da"] .. hiragana["sa"] .. hiragana["i"] .. "!" 
 question[20].a1 = "art/Game3/bread.png"
 question[20].a2 = "art/Game3/coffee.png"
 question[20].a3 = "art/Game3/water.png"
